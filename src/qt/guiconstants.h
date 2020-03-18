a8e1291675748a85ff5f3e2408e34e67a1270658
NullFunctor <Null@Functor.com>
2020-03-18 09:36:14 -0500

    default qt settings

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -42,11 +42,11 @@ static const int TOOLTIP_WRAP_THRESHOLD = 80;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
-#define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
+#define QAPP_ORG_NAME "iBitcoin"
+#define QAPP_ORG_DOMAIN "iBTC.network"
+#define QAPP_APP_NAME_DEFAULT "iBitcoin-Qt"
+#define QAPP_APP_NAME_TESTNET "iBitcoin-Qt-testnet"
+#define QAPP_APP_NAME_REGTEST "iBitcoin-Qt-regtest"
 
 /* One gigabyte (GB) in bytes */
 static constexpr uint64_t GB_BYTES{1000000000};
